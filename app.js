d49a94d09be83deb339f9fe9311d866a5b724659
Rebecca Ross <b.laurenross@gmail.com>
2021-01-19 01:18:39 +0000

    created reusable fetch function

--- app.js
+++ app.js
@@ -3,31 +3,51 @@ const card = document.querySelector('.card');
 
 
 //Fetch functions
-fetch('https://dog.ceo/api/breeds/image/random')
-  .then(response => response.json())
+
+//Reusable fetch function
+function fetchData(url) {
+  return fetch(url)
+    .then(res => res.json())
+}
+
+fetchData('https://dog.ceo/api/breeds/image/random')
   .then(data => showImage(data.message))
 
-fetch('https://dog.ceo/api/breeds/list')
-  .then(response => response.json())
+fetchData('https://dog.ceo/api/breeds/list')
   .then(data => showOptions(data.message))
+
 
 //Helper functions
 
 function showImage(data) {
   const html = `
     <img src='${data}' alt>
-    <span>Click to view more images of ${select.value}</span>
+    <span>Click to view more ${select.value}s</span>
   `;
   card.innerHTML = html;
 }
 
 function showOptions(data) {
   const options = data.map(item => `
-    <option value='${item}'>${item}</option>
-    `);
+    <option value='${item}'>${item.toUpperCase()}</option>
+    `).join('');
   select.innerHTML = options;
 }
 
+
+function fetchBreedImage() {
+  const breed = select.value;
+  const img = card.querySelector('img');
+  const span = card.querySelector('span');
+
+  fetchData(`https://dog.ceo/api/breed/${breed}/images/random`)
+    .then(data => {
+      img.src = data.message;
+      img.alt = breed;
+      span.textContent = `Click to view more ${breed}s`;
+    })
+}
+
 //Event listeners
-
-
+select.addEventListener('change', fetchBreedImage);
+card.addEventListener('click', fetchBreedImage);
